b0f812e62ce17ebe1ccf9755c997ba84a733218c
Johan Halin <johan@aerodeko.com>
2012-08-05 15:08:41 +0300

    Removed unused delegate methods.

--- Classes/CircloAppDelegate.m
+++ Classes/CircloAppDelegate.m
@@ -16,8 +16,7 @@
 @synthesize viewController;
 
 
-#pragma mark -
-#pragma mark Private
+#pragma mark - Private
 
 - (void)_createUserDefaults
 {
@@ -30,8 +29,7 @@
 }
 
 
-#pragma mark -
-#pragma mark Application lifecycle
+#pragma mark - Application lifecycle
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {    
@@ -44,20 +42,8 @@
 }
 
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    /*
-     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-     */
-}
-
-
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    /*
-     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-     If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
-     */
 	[self.viewController stopAllCircleAnimations];
 }
 
@@ -66,33 +52,6 @@
 {
 	[self.viewController startAllCircleAnimations];
 }
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    /*
-     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-     */
-}
-
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    /*
-     Called when the application is about to terminate.
-     See also applicationDidEnterBackground:.
-     */
-}
-
-
-#pragma mark -
-#pragma mark Memory management
-
-- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
-    /*
-     Free up as much memory as possible by purging cached data objects that can be recreated (or reloaded from disk) later.
-     */
-}
-
-
 
 
 @end
