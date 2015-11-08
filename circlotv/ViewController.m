f7e4b19fe95cdd367fabb3c114d1e27ea16cab12
Johan Halin <johan@halin.me>
2015-11-08 07:59:54 +0200

    Added tvOS target. Reorganized project structure a bit.

---
+++ circlotv/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  circlotv
+//
+//  Created by Johan Halin on 8.11.2015.
+//  Copyright © 2015 Aero Deko. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
